6ee5d1945d0946117f66deabb09d96900b73d048
Daniel Pinna <danielsouzapinna@outlook.com.br>
2020-09-26 17:46:49 -0300

    Reset CSS and defining global css

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyles from './styles/global';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <GlobalStyles />
+  </>,
+  document.getElementById('root'),
 );
