21349a33a6d2925a2d7408ee22215190467919a4
Daniel Pinna <danielsouzapinna@outlook.com.br>
2020-09-27 17:59:26 -0300

    Adding new component

---
+++ src/components/TableAppointmentsPatient/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+interface PatientDetail {
+  name: string;
+}
+
+interface Appointment {
+  id: string;
+  date: string;
+  note: string;
+  patient: PatientDetail;
+}
+
+interface TableAppointmentsPatientProps {
+  headers: string[];
+  rows: Appointment[];
+}
+
+const TableAppointmentsPatient: React.FC<TableAppointmentsPatientProps> = ({ headers, rows }: TableAppointmentsPatientProps) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {headers.map((item: string) => (
+            <th key={item}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((item: Appointment) => (
+          <tr key={item.id}>
+            <td>{item.date}</td>
+            <td>{item.note}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableAppointmentsPatient;
