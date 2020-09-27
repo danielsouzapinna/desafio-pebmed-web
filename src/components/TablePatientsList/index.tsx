5240bb57e4d5b893aca28427ed85f29afd7b4a09
Daniel Pinna <danielsouzapinna@outlook.com.br>
2020-09-27 00:26:22 -0300

    Adding component TablePatientList

---
+++ src/components/TablePatientsList/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Table from 'react-bootstrap/Table';
+
+interface Patient {
+  name: string;
+  dateBirth: string;
+  gender: string;
+  phone: string;
+}
+
+interface TablePatientsListProps {
+  headers: string[];
+  rows: Patient[];
+}
+
+const TablePatientsList: React.FC<TablePatientsListProps> = ({ headers, rows }: TablePatientsListProps) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          {headers.map((item: string) => (
+            <th>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((item: Patient) => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.dateBirth}</td>
+            <td>{item.gender}</td>
+            <td>{item.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TablePatientsList;
--- src/pages/Patients/index.tsx
+++ src/pages/Patients/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import TablePatientsList from '../../components/TablePatientsList';
 
 const Patients: React.FC = () => {
-  return <h1>Patients</h1>;
+  const labels = ['Nome', 'Data Nascimento', 'Sexo', 'Telefone'];
+  const values = [
+    { name: 'João', dateBirth: '99/99/9999', gender: 'Masculino', phone: '(99) 99999-9999' },
+    { name: 'Pedro', dateBirth: '99/99/9999', gender: 'Masculino', phone: '(99) 99999-9999' },
+    { name: 'Maria', dateBirth: '99/99/9999', gender: 'Masculino', phone: '(99) 99999-9999' },
+  ];
+
+  return (
+    <>
+      <h1>Pacientes</h1>
+      <TablePatientsList headers={labels} rows={values} />
+    </>
+  );
 };
 
 export default Patients;
