ebe4b7b1ee8887e19069a935b3f5d456bdb7c080
Daniel Pinna <danielsouzapinna@outlook.com.br>
2020-09-26 23:42:02 -0300

    Adding menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Menu from './components/Menu';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import GlobalStyles from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <Router>
+    <Menu />
     <Routes />
     <GlobalStyles />
   </Router>
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.png';
+
+const Menu: React.FC = () => {
+  return (
+    <Navbar bg="secondary" variant="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">
+        <img src={logoImg} alt="Logo ProntoMED" width="30" height="30" className="d-inline-block align-top" />
+        ProntoMED
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/patients">
+            Pacientes
+          </Nav.Link>
+          <Nav.Link as={Link} to="/aapointments">
+            Agendamentos
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Menu;
