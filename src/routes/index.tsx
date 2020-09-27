58544c9a04bc0e8536456e5985b98e503a95470f
Daniel Pinna <danielsouzapinna@outlook.com.br>
2020-09-27 17:55:04 -0300

    Adding new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Appointments from '../pages/Appointments';
 import Dashboard from '../pages/Dashboard';
 import Patients from '../pages/Patients';
+import PatientDetail from '../pages/PatientDetail';
+import AddPatient from '../pages/AddPatient';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/appointments" component={Appointments} />
     <Route path="/patients" component={Patients} />
+    <Route path="/patient-detail" component={PatientDetail} />
+    <Route path="/add-patient" component={AddPatient} />
   </Switch>
 );
 
