25ad7f7c4e4959f6f219e9c7ad164b172c327a64
Daniel Pinna <danielpinna2@gmail.com>
2020-10-04 13:15:36 -0300

    adding modal

--- src/components/TableAppointments/index.tsx
+++ src/components/TableAppointments/index.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import React, { useState } from 'react';
 import Table from 'react-bootstrap/Table';
+import format from 'date-fns/format';
+import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
+
+import ModalPatient from '../ModalPatient';
+import API from '../../services/api';
 
 interface PatientDetail {
   name: string;
@@ -17,24 +24,62 @@ interface TableAppointmentsProps {
 }
 
 const TableAppointments: React.FC<TableAppointmentsProps> = ({ headers, rows }: TableAppointmentsProps) => {
+  const [id, setId] = useState('');
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(!show);
+  const history = useHistory();
+
+  const handleDelete = (id: string) => {
+    setId(id);
+    handleShow();
+  };
+
+  const removeAppointment = () => {
+    API.delete(`appointments/${id}`)
+      .then(result => {
+        if (result.status === 204) {
+          const filteredAppointments = rows.filter(item => {
+            return item.id !== id;
+          });
+
+          rows = filteredAppointments;
+          history.push('/');
+          history.push('/appointments');
+          handleShow();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          {headers.map((item: string) => (
-            <th key={item}>{item}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((item: Appointment) => (
-          <tr key={item.id}>
-            <td>{item.patient.name}</td>
-            <td>{item.date}</td>
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            {headers.map((item: string) => (
+              <th key={item}>{item}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {rows.map((item: any) => (
+            <tr key={item.id}>
+              <td>{item.patient.name}</td>
+              <td>{format(new Date(item.date), 'dd-MM-yyyy')}</td>
+              <td>
+                <Button variant="danger" size="sm" onClick={() => handleDelete(item.id)}>
+                  Remover
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      <ModalPatient modalShow={show} title="Excluir Consulta" text="Tem certeza que deseja excluir a consulta?" action={removeAppointment} handleClose={handleShow} />
+    </>
   );
 };
 
